e605a782c1950cd1a733de4aee9274f07ac1692a
murrie030 <mustafa_2000_gs@live.nl>
2020-04-07 15:27:58 +0200

    Unit testen, coverage en parameterized tests

--- src/main/java/domain/rules/RockPaperScissors.java
+++ src/main/java/domain/rules/RockPaperScissors.java
@@ -7,32 +7,16 @@ import domain.Rules;
 public class RockPaperScissors implements Rules {
     @Override
     public Result decide(Move mine, Move theirs) {
-        switch (mine) {
-            case ROCK:
-                switch (theirs) {
-                    case SCISSORS:
-                        return Result.WIN;
-                    case PAPER:
-                        return Result.LOSE;
-                }
-
-            case PAPER:
-                switch (theirs) {
-                    case ROCK:
-                        return Result.WIN;
-                    case SCISSORS:
-                        return Result.LOSE;
-                }
-
-            case SCISSORS:
-                switch (theirs) {
-                    case PAPER:
-                        return Result.WIN;
-                    case ROCK:
-                        return Result.LOSE;
-                }
+        if ((mine == Move.ROCK && theirs == Move.SCISSORS) ||
+                (mine == Move.SCISSORS && theirs == Move.PAPER) ||
+                    (mine == Move.PAPER && theirs == Move.ROCK)) {
+            return Result.WIN;
+        } else if ((mine == Move.ROCK && theirs == Move.ROCK) ||
+                        (mine == Move.SCISSORS && theirs == Move.SCISSORS) ||
+                            (mine == Move.PAPER && theirs == Move.PAPER)) {
+            return Result.DRAW;
+        } else {
+            return Result.LOSE;
         }
-
-        return Result.DRAW;
     }
 }
